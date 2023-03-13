b550a23d368aeb6182d8ac3136892f314b47977a
Felipe Marcon <felipeemarcon@gmail.com>
2023-03-13 00:54:39 -0300

    feat: get products from database in home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Contracts\View\View;
 
 class HomeController extends Controller
 {
     public function index(): View
     {
-        return view('home');
+        $products = Product::paginate(8);
+        return view('home', compact('products'));
     }
 }
