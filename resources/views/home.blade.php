4b3ea2bca44f2c1fc5ae2ee747484e36374f9406
Felipe Marcon <felipeemarcon@gmail.com>
2023-03-13 00:25:36 -0300

    feat: make header and footer as partials

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <div class="container px-5 py-24 mx-auto">
             <div class="flex flex-wrap -m-4">
                 <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                    <a class="block relative h-48 rounded overflow-hidden">
+                    <a href="{{ route('product.show', 1) }}" class="block relative h-48 rounded overflow-hidden">
                         <img alt="ecommerce" class="object-cover object-center w-full h-full block"
                             src="https://dummyimage.com/800x450">
                     </a>
