b686660b3135df4d1b22ea59e5c4b9db0b040a5d
Felipe Marcon <felipeemarcon@gmail.com>
2023-03-16 13:51:21 -0300

    feat: add to cart button to product pag

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -9,8 +9,11 @@
                     src="{{ $product->image }}">
                 <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                     <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{{ $product->title }}</h1>
+                    <div class="flex border-t-2 border-gray-100 mt-6 py-6">
+                        <span class="title-font font-medium text-2xl text-gray-900">${{ $product->price }}</span>
+                    </div>
                     <p class="leading-relaxed">{{ $product->description }}</p>
-                    <div class="my-3">
+                    <div class="my-5">
                         @if ($product->stock)
                             <span
                                 class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-indigo-100 text-indigo-800">Em
@@ -22,11 +25,12 @@
                         @endif
                     </div>
 
-                    <div class="flex border-t-2 border-gray-100 mt-6 pt-6">
-                        <span class="title-font font-medium text-2xl text-gray-900">${{ $product->price }}</span>
+                    <div class="flex flex-col border-t-2 border-gray-100 pt-6 gap-3">
+                        <a
+                                class="flex justify-center cursor-pointer text-slate-700 bg-slate-200 border-0 py-4 px-6 focus:outline-none hover:bg-slate-300 rounded">Add to cart</a>
                         @if ($product->stock)
                             <a
-                                class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Buy</a>
+                                class="flex justify-center cursor-pointer text-white bg-indigo-500 border-0 py-4 px-6 focus:outline-none hover:bg-indigo-600 rounded">Buy</a>
                         @endif
                     </div>
                 </div>
